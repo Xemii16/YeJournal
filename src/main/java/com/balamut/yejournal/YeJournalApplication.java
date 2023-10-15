39f86d71c720eac59d06e41d3ecabc5ee6624d9f
Xemii16 <balamutmaks@gmail.com>
2023-10-15 15:49:19 +0300

    Initial commit

---
+++ src/main/java/com/balamut/yejournal/YeJournalApplication.java
@@ -0,0 +1,13 @@
+package com.balamut.yejournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeJournalApplication.class, args);
+    }
+
+}
