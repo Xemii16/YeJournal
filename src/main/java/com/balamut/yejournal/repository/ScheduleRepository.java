60325d4fa53f7160ad4ba025324110b3fc1e0447
Maks Balamut <balamutmaks@gmail.com>
2023-11-01 20:38:07 +0200

    Delete unnecessary classes

--- src/main/java/com/balamut/yejournal/repository/ScheduleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.balamut.yejournal.repository;
-
-import com.balamut.yejournal.entity.Schedule;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface ScheduleRepository extends CrudRepository<Schedule, UUID> {
-}
